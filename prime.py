a7fb1b290c7b08898e188fbb98d97e0e32917e6b
Tech Skills Transformations LLC <82771267+techupskills@users.noreply.github.com>
2025-05-12 14:55:52 -0400

    Create prime.py

---
+++ prime.py
@@ -0,0 +1,15 @@
+def is_prime(n):
+    """Check if a number is prime."""
+    if n <= 1:
+        return False
+    elif n <= 3:
+        return True
+    elif n % 2 == 0 or n % 3 == 0:
+        return False
+
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i += 6
+    return True
